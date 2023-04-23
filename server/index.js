edfd870d1a97ed03df01d8c3ed237b0211b70c1c
Ayush <ayush4002gupta@gmail.com>
2023-04-23 18:12:58 +0530

    connected to database/ subscribe API complete

--- server/index.js
+++ server/index.js
@@ -3,8 +3,13 @@ const cors = require('cors')
 const db =require('./db');
 const PORT = 5000;
 const app = express();
+const routee = require('./routes/routes');
+const router = express.Router();
 app.use(express.json());
 app.use(cors());
+
+
+
 
 app.get("/",(req,res) => {
     res.json({
@@ -17,12 +22,50 @@ app.post("/chat",(req,res) => {
     const name= req.body.name;
     const email = req.body.email;
     const organization = req.body.organization;
+    const date= new Date().toISOString();
+    console.log(date)
 
-    res.json({
-        college :organization,
-        name: name,
-        email: email
-    });
+
+    try {
+        db.query("SELECT * FROM users WHERE email=? ", [email], (err, result) => {
+          if (err) {
+            console.log(err)
+          }
+          if (result.length > 0) {
+            res.json({
+               messae:"you are already subscribed"
+            });
+           
+          }
+           else {
+            
+            db.query("INSERT INTO users (name,email,organization,date)  VALUES (?, ?, ?,?)", [name,email,organization,date], (err, result) => {
+              if (err) {
+                console.log(err)
+              }
+              console.log(err)
+              res.json({
+                message:"you are subscribed"
+            });
+              
+              
+            });
+          }
+       
+        })
+    
+
+      } catch (error) {
+        console.log(error);
+        
+      }
+
+
+
+
+
+
+
    
 });
 

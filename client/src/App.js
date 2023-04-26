53808b7dd15edd6ff35ae02731589cd62e6d0485
Ayush <ayush4002gupta@gmail.com>
2023-04-26 22:52:26 +0530

    made counter page responsive

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <Hero/>
-      <About/>
+      {/* <About/> */}
       <Count/>
       <Testimonials/>
     </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,28 @@
 module.exports = {
   content: [ "./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+      'xmd': {'max': '900px'},
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+      'nm': {'max': '680px'},
+
+      'xnm':{'max':'860px'},
+
+      'sm': {'max': '580px'},
+      // => @media (max-width: 639px) { ... }
+
+      'xsm': {'max': '380px'},
+    },
     extend: {},
   },
   plugins: [],

033851bee7614803b19537c11c215318d134c456
Ayush <ayush4002gupta@gmail.com>
2023-04-27 16:41:30 +0530

    footer improved

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -3,38 +3,45 @@ import "../CSS/Footer.css"
 
 export default function Footer() {
 
-  return (
-    <>
-    <div className="feedback-con">
-        
-        <div className="feedback ">
-            <h3>
-                Your feedback is highly valuable to us.
-            </h3>
+    return (
+        <>
+            <div className="feedback-con">
 
-            <div className="hr" style={{"width": "10%"}}></div>
+                <div className="feedback ">
+                    <h3>
+                        Your feedback is highly valuable to us.
+                    </h3>
 
-            <form action="" method="post" className='feedback-form mt-3' id='feedback-form'>
-                {/* <h2>Feedback</h2> */}
-                <textarea name="feedback" id="feedback" placeholder='Your feedback'></textarea>
-                {/* <input type="text" name="feedback" id="feedback" placeholder='Your feedback'/> */}
-                <button type="submit">SEND</button>
-            </form>
-        </div>
+                    <div className="hr" style={{ "width": "10%" }}></div>
 
-        <div className="feedback-doodle">
-            <img src={require("../Assets/feedback.png")} alt="" />
-        </div>
-    </div>
-    <div className='footer'>
+                    <form action="" method="post" className='feedback-form mt-3 ' id='feedback-form'>
+                        {/* <h2>Feedback</h2> */}
+                        <textarea name="feedback" id="feedback" placeholder='Your feedback'></textarea>
+                        {/* <input type="text" name="feedback" id="feedback" placeholder='Your feedback'/> */}
+                        <button type="submit">SEND</button>
+                    </form>
+                </div>
 
-        <div className="footer-title">
-        <img src={require("../Assets/devfeed-logo2.png")} alt="" id='footer-logo' />
-        </div>
-        <div className="copyright">
-            &copy; Copyrights by DevFeed. All rights reserved 
-        </div>
-    </div>
-    </>
-  )
+                <div className="feedback-doodle  nm:hidden">
+                    <img src={require("../Assets/feedback.png")} alt="" />
+                </div>
+            </div>
+
+            <div className='footer'>
+            <div className="hr" style={{ "width": "90%" , "marginTop":"3vh"}}></div>
+                <div className='footer-main'>
+                    <div className="footer-title">
+                        {/* <img src={require("../Assets/devfeed-logo2.png")} alt="" id='footer-logo' /> */}
+                        DevFeed
+                    </div>
+                    <div className="copyright">
+                        &copy; Copyrights by DevFeed. All rights reserved
+                    </div>
+                </div>
+
+            
+                
+            </div>
+        </>
+    )
 }

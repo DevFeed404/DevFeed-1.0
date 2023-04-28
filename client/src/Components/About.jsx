bcd420b76f7f3eaced608ae04c9248c0d5e12a86
Ayush <ayush4002gupta@gmail.com>
2023-04-28 21:57:39 +0530

    added about page

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -4,7 +4,7 @@ import "../CSS/About.css"
 export default function About() {
   return (
     <>
-    <div className='about'>
+      {/* <div className='about'>
         <div className="vision-headline">OUR VISION</div>
         <div className="vision">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto numquam vitae aperiam ipsa obcaecati iste soluta officia.
@@ -16,7 +16,34 @@ export default function About() {
         <img src="https://wp-media-partyslate.imgix.net/2021/01/photo-4e533484-e9fb-4444-9894-b10a2fa15657.jpg?auto=compress%2Cformat&ixlib=php-3.3.1" alt="" id="about-photo-1" />
         <img src="https://partyslate.imgix.net/photos/614328/photo-b7c1902f-20e8-4abd-b3f9-46477b84190a.JPG?ixlib=js-2.3.2&auto=compress%2Cformat&w=400" alt="" id="about-photo-2" />
         <img src="https://www.installation-international.com/wp-content/uploads/2019/01/airstar-neo-e-sports.jpg" alt="" id="about-photo-3" />
-    </div>
+    </div> */}
+
+      <div className='h-fit flex flex-row w-screen background mh-[2vh]' >
+        <div className='w-[50%] mlg:hidden'>
+          <img src={require("../Assets/about.png")} alt="" className='about-image' />
+
+
+        </div>
+        <div className='flex flex-col w-[50%] mlg:w-[80%] mr-[4vw]'>
+          <h1 className='mt-[9vh] xlg:mt-[2vh] ml-[5vw] sm:text-4xl xsm:text-3xl text-5xl font-mono items-center font-bold text-white'>Nice to meet you</h1>
+          <p className='text-xl mt-[3vh]  text-white font-normal font-sans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis Ut enim ad minim veniam, quis  Ut enim ad minim veniam, quis  nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          <ul class="list-disc ml-[1.2vw] pt-3 text-lg text-white">
+  <li className='pt-3'>Now this is a story all about how, my life got flipped-turned upside down</li>
+  <li className='pt-3'>Now this is a story all about how, my life got flipped-turned upside down</li>
+  <li className='pt-3'>Now this is a story all about how, my life got flipped-turned upside down</li>
+  <li className='pt-3'>Now this is a story all about how, my life got flipped-turned upside down</li>
+  
+
+</ul>
+
+
+
+
+
+
+        </div>
+
+      </div>
     </>
   )
 }

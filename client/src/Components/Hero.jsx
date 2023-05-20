57b158559f9ca7cfa81083a3a4faffa6449e2529
subhabrata <malliksubhabrata@gmail>
2023-05-20 13:42:19 +0530

    made changes in image.jsx

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -13,8 +13,8 @@ export default function Hero() {
   return (
     <div className='hero'>
         <div className="hero-data">
-            <div className="hero-head">
-            Educate. <p>Connect. </p> Aware. 
+            <div className="hero-head animation">
+            Educate. <p> Connect.</p> Aware. 
             </div>
             <div className="hero-text">
            <span className='text-[#28004d] comme'>what are you waiting for? </span> Join our community and be a part of the tech revolution! 

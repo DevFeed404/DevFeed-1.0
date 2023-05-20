fa6fc6a57569edfc84d240a8f9bf0f58f460a404
Rishu <rrishu561@gmail.com>
2023-05-21 00:35:36 +0530

    added css to subscribe button and to navbar

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,24 +1,37 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "../CSS/Navbar.css";
 
 export default function Navbar() {
-    return (
-        <div className="nav-body">
-            <a href="#home">
-            <div className="logo">
-                <img
-                    src={require("../Assets/logoimage.png")}
-                    alt=""
-                    id="nav-logo"
-                />
-            </div>
-            </a>
-            <a href="#subscribe-con">
-                <div className="subscribe font-medium">
-                    Subscribe
-                    {/* <img src={require("../Assets/right.png")} alt=""  /> */}
-                </div>
-            </a>
+  const [navbar, setNavbar] = useState(false);
+
+  const changeBackground = () => {
+    console.log(window.scrollY);
+    if (window.scrollY >= 66) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  useEffect(() => {
+    changeBackground();
+    window.addEventListener("scroll", changeBackground);
+  });
+
+  return (
+    <div className={navbar ? "nav-body--scrolled" : "nav-body"}>
+      <a href="#home">
+        <div className="logo">
+          <img src={require("../Assets/logoimage.png")} alt="" id="nav-logo" />
         </div>
-    );
+      </a>
+      <a href="#subscribe-con">
+        <div className="subscribe font-medium">
+          Subscribe
+          {/* <img src={require("../Assets/right.png")} alt=""  /> */}
+        </div>
+      </a>
+    </div>
+  );
 }

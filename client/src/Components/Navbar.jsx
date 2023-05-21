d9d0bb5ad4bff4e429ca8edaffe119b996f720b7
Subham Bhattacharjee <subhambhttachariya03@gmail.com>
2023-05-21 22:13:49 +0530

    Added Preloading Animation

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import "../CSS/Navbar.css"
+import React from 'react';
+import "../CSS/Navbar.css";
+
 
 export default function Navbar() {
 
-  function scrollToTop(){
-    window.scrollTo({'top': 0, "behavior": "smooth"});
+  function scrollToTop() {
+    window.scrollTo({ 'top': 0, "behavior": "smooth" });
     console.log("Scroll to top");
   }
 
-  function scrollToSubs(){
+  function scrollToSubs() {
     var element = document.getElementById("subscribe-con");
     element.scrollIntoView();
   }
 
   return (
     <div className='nav-body'>
-        <div className="logo">
-            <img src={require("../Assets/logoimage.png")} alt="" id='nav-logo' onClick={scrollToTop}/>
-        </div>
-        <div className="subscribe font-medium" onClick={scrollToSubs}>
-            Subscribe 
-            {/* <img src={require("../Assets/right.png")} alt=""  /> */}
-        </div>
+      <div className="logo">
+        <img src={require("../Assets/logoimage.png")} alt="" id='nav-logo' onClick={scrollToTop} />
+      </div>
+      <div className="subscribe font-medium" onClick={scrollToSubs}>
+        Subscribe
+        {/* <img src={require("../Assets/right.png")} alt=""  /> */}
+      </div>
     </div>
   )
 }

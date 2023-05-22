f652c27aba3c8aa006492d70ae3d9babe5d75ba3
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2023-05-22 13:06:03 +0530

    Code Refactor

---
+++ client/src/utils/TestimonialData.js
@@ -0,0 +1,36 @@
+const testimonialList = [
+    {
+        img_src: "../Assets/himesh.jpg",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti sunt odio necessitatibus aliquam doloremque incidunt, quo id alias optio laborum? ",
+        name: "ipsum dolor",
+        email: "xyz@gmail.com"
+    },
+    {
+        img_src: "../Assets/himesh.jpg",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti sunt odio necessitatibus aliquam doloremque incidunt, quo id alias optio laborum? ",
+        name: "ipsum dolor",
+        
+        email: "xyz@gmail.com"
+    },
+    {
+        img_src: "../Assets/himesh.jpg",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti sunt odio necessitatibus aliquam doloremque incidunt, quo id alias optio laborum? ",
+        name: "ipsum dolor",
+        email: "xyz@gmail.com"
+    },
+    {
+        img_src: "../Assets/himesh.jpg",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti sunt odio necessitatibus aliquam doloremque incidunt, quo id alias optio laborum? ",
+        name: "ipsum dolor",
+        email: "xyz@gmail.com"
+    },
+    {
+        img_src: "../Assets/himesh.jpg",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti sunt odio necessitatibus aliquam doloremque incidunt, quo id alias optio laborum? ",
+        name: "ipsum dolor",
+        email: "xyz@gmail.com"
+    },
+
+]
+
+export default testimonialList;

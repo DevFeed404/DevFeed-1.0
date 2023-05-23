fc0afb7cb5b716d842e6c63524ef92c7bdb49269
Chukwuebuka2 <nwokporochukwuebuka@gmail.com>
2023-05-23 18:03:27 +0100

    UPDATE: Added joi Validation'

---
+++ server/middlewares/validate.js
@@ -0,0 +1,22 @@
+const Joi = require("joi");
+const pick = require("../utils/pick");
+const ApiError = require("../utils/ApiError");
+
+const validate = (schema) => (req, res, next) => {
+  const validSchema = pick(schema, ["params", "query", "body"]);
+  const object = pick(req, Object.keys(validSchema));
+  const { value, error } = Joi.compile(validSchema)
+    .prefs({ errors: { label: "key" }, abortEarly: false })
+    .validate(object);
+
+  if (error) {
+    const errorMessage = error.details
+      .map((details) => details.message)
+      .join(", ");
+    return next(new ApiError(400, errorMessage));
+  }
+  Object.assign(req, value);
+  return next();
+};
+
+module.exports = validate;

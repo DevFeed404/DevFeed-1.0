9945ec37f0eb77e2710533120ce32db6e7fd4601
An525ish <an525ish@gmail.com>
2023-05-25 21:36:37 +0530

    make it responsive

--- client/src/Components/Testimonials.jsx
+++ client/src/Components/Testimonials.jsx
@@ -4,21 +4,7 @@ import testimonialList from "../utils/TestimonialData";
 import Carousel from './Carousel'
 
 export default function Testimonials() {
-  let vw = window.screen.width / 100;
-  // let vh = window.Screen.height / 100;
-  // var vw = window.screen.availWidth;
-  // const vh = screen.height;
-  // console.log("Height is :", vw);
-  function scrollLeft() {
-    var element = document.getElementById("testimonials-body");
-    element.scrollLeft += 30 * vw;
-  }
-
-  function scrollRight() {
-    var element = document.getElementById("testimonials-body");
-    element.scrollLeft -= 30 * vw;
-  }
-
+  
   return (
     <>
       <div className="testimonials-title  sans-serif font-bold text-center">

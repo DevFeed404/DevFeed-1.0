71cc71815de1bc2e20074ead5ada7a7e3417aeea
Madhur Rastogi <rastogi.madhur87@gmail.com>
2023-05-28 07:38:10 +0530

    Add Joi Validation

--- server/controllers/Feedback.js
+++ server/controllers/Feedback.js
@@ -1,26 +1,30 @@
 const db = require('../db');
 require("dotenv").config();
+const feedbackSchema = require('../validation/Feedback');
 
 const validator = require("validator")
 exports.feedback = async (req, res) => {
-    const feedback =req.body.feedback;
-    try {
-        if (validator.isEmpty(feedback)) {
-            res.send({ message: 'Feedback is empty' });
-      
-          }if(validator.isEmpty(feedback)==false){
+  try {
+    const payload = await feedbackSchema.validateAsync(req.body);
+    if (!payload.error) {
+      const feedback = req.body.feedback;
+      if (validator.isEmpty(feedback)) {
+        res.send({ message: 'Feedback is empty' });
+
+      } if (validator.isEmpty(feedback) == false) {
         db.query("INSERT INTO feedback (feedback)  VALUES (?) ", [feedback], (err, result) => {
-            if (err) {
-              console.log(err)
-            }
-            res.send({result, message:"Thankyou for your feedback"});
-            console.log(result);
+          if (err) {
+            console.log(err)
+          }
+          res.send({ result, message: "Thankyou for your feedback" });
+          console.log(result);
         })
+      }
+    } else {
+      return res.status(400).send({ message: payload.error.message });
     }
-             
-        
-        
-    } catch (error) {
-        
-    }
+  }
+  catch (err) {
+    console.log(err);
+  }
 }
--- server/controllers/Sendtome.js
+++ server/controllers/Sendtome.js
@@ -1,50 +1,53 @@
 const db = require('../db');
 var nodemailer = require('nodemailer');
 require("dotenv").config();
+const sendSchema = require('../validation/Send');
 
 exports.sendtome = async (req, res) => {
     try {
-        var message = req.body.message;
-        var title = req.body.title;
+        const payload = await sendSchema.validateAsync(req.body);
+        if (!payload.error) {
+            const { message, title } = payload;
 
-        var transport = nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            port: 587,
-            auth: {
-                user: process.env.EMAIL_SENDER,
-                pass: process.env.EMAIL_PASSWORD
-            },
-            tls: {
-                rejectUnauthorized: false
-            }
-        });
+            var transport = nodemailer.createTransport({
+                host: "smtp.gmail.com",
+                port: 587,
+                auth: {
+                    user: process.env.EMAIL_SENDER,
+                    pass: process.env.EMAIL_PASSWORD
+                },
+                tls: {
+                    rejectUnauthorized: false
+                }
+            });
 
-        var mailOptions = {
-            from: `"DevFeed.in" <${process.env.EMAIL_SENDER}>`,
-            to: `${process.env.EMAIL_ME},${process.env.EMAIL_ME2}`,
-       
-            subject: `${title}`,
-            text: `Hey there, Welcome to DevFeed.in , Your daily solution to keep you updated`,
-            html: `${message}`,
+            var mailOptions = {
+                from: `"DevFeed.in" <${process.env.EMAIL_SENDER}>`,
+                to: `${process.env.EMAIL_ME},${process.env.EMAIL_ME2}`,
 
-        };
+                subject: `${title}`,
+                text: `Hey there, Welcome to DevFeed.in , Your daily solution to keep you updated`,
+                html: `${message}`,
 
-        transport.sendMail(mailOptions, (error, info) => {
-            if (error) {
+            };
 
-                return console.log(error);
-            }
-            console.log(info);
-            res.send({message:"sent"})
-            console.log('Message sent: %s', info.messageId);
-        });
+            transport.sendMail(mailOptions, (error, info) => {
+                if (error) {
 
-
+                    return console.log(error);
+                }
+                console.log(info);
+                res.send({ message: "sent" })
+                console.log('Message sent: %s', info.messageId);
+            });
+        } else {
+            return res.status(400).send({ message: payload.error.message });
+        }
     }
     catch (error) {
         console.log(error);
 
 
-  }; 
+    };
 };
 

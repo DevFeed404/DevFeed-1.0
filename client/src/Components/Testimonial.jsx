5f29d1c5fa77675610a9730af2dc042dd5d0f314
Vishal K <116670999+Vishalk91-4@users.noreply.github.com>
2023-06-03 01:03:18 +0530

    Update Testimonial.jsx
    Signed-off-by: Vishal K <116670999+Vishalk91-4@users.noreply.github.com>

--- client/src/Components/Testimonial.jsx
+++ client/src/Components/Testimonial.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import "../CSS/Testimonial.css"
 
+
 export default function Testimonial(props) {
   return (
     <div className='Testimonial'>
-        <img src={require("../Assets/himesh.jpg")} alt="" className='testimonial-img' />
-        <div className="testimonial-text sans-serif">
-            {props.text}
-        </div>
-        <hr style={{"BackgroundColor": "white", "width": "30%", "marginTop": "2vh"}} />
-        <hr style={{"BackgroundColor": "white", "width": "30%"}} />
-        <hr style={{"BackgroundColor": "white", "width": "30%", "marginBottom": "2vh"}} />
+      <div className='content'>
+        <img src={require("../Assets/chloe.jpg")} alt="" className='testimonial-img' />
         <div className="testimonial_name sans-serif" >
             {props.name}
         </div>
         <div className="testimonial_email sans-serif">
             {props.email}
         </div>
+        <div className="testimonial-text sans-serif">
+            {props.text}
+        </div>
+      </div>
 
     </div>
   )

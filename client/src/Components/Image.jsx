f371124b39e2f513e444dcd69be125080bb2d040
Mridul Mittal <mridulmittal2003@gmail.com>
2023-06-02 19:21:02 +0530

    changes image.jsx hover effect

--- client/src/Components/Image.jsx
+++ client/src/Components/Image.jsx
@@ -10,11 +10,11 @@ function Images() {
           <img
             src={require(`../Assets/${images.imageName}`)}
             alt=""
-            className={
+            className= {
               index === 0
                 ? // Added transition, margin, cursor also added the hover effect into the card image
-                  "width pl-2 m-2 cursor-pointer hover:filter hover:brightness-77 transition duration-300 transform hover:scale-105"
-                : "width m-2 cursor-pointer hover:filter hover:brightness-77 transition duration-300 transform hover:scale-105"
+                  "width pl-2 m-2 cursor-pointer hover:filter hover:brightness-77 ransition ease-in-out delay-150  hover:-translate-y-1 hover:scale-150  duration-300 ..."
+                : "width m-2 cursor-pointer hover:filter hover:brightness-77 ransition ease-in-out delay-150  hover:-translate-y-1 hover:scale-150 duration-300 ..."
             }
             key={index}
           />

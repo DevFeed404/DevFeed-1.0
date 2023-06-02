636b3d83dcdd1390860c971aeb5f3c3bec3d7113
Vishal K <116670999+Vishalk91-4@users.noreply.github.com>
2023-06-03 01:05:47 +0530

    Update TestimonialData.js
    Signed-off-by: Vishal K <116670999+Vishalk91-4@users.noreply.github.com>

--- client/src/utils/TestimonialData.js
+++ client/src/utils/TestimonialData.js
@@ -1,36 +1,37 @@
 const testimonialList = [
     {
-        img_src: "../Assets/himesh.jpg",
-        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti sunt odio necessitatibus aliquam doloremque incidunt, quo id alias optio laborum? ",
-        name: "ipsum dolor",
-        email: "xyz@gmail.com"
+        img_src: "../Assets/chloe.jpg",
+        text: "Since subscribing to the technical developer newsletter, my knowledge and skills have grown exponentially. ",
+        name: "Alya Abbot",
+        email: "alyaabbot@gmail.com"
     },
     {
-        img_src: "../Assets/himesh.jpg",
-        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti sunt odio necessitatibus aliquam doloremque incidunt, quo id alias optio laborum? ",
-        name: "ipsum dolor",
+        img_src: "../Assets/chloe.jpg",
+        text: "The concise and well-structured articles provide me with the knowledge I need without overwhelming me with unnecessary details",
+        name: "Chris Taylor",
         
-        email: "xyz@gmail.com"
+        email: "chrisedu@gmail.com"
     },
     {
-        img_src: "../Assets/himesh.jpg",
-        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti sunt odio necessitatibus aliquam doloremque incidunt, quo id alias optio laborum? ",
-        name: "ipsum dolor",
-        email: "xyz@gmail.com"
+        img_src: "../Assets/chloe.jpg",
+        text: " It's a treasure trove of practical tips, in-depth tutorials, and thought-provoking industry analyses.",
+        name: "Alyssa Healey",
+        email: "alyssahvd@gmail.com"
     },
     {
-        img_src: "../Assets/himesh.jpg",
-        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti sunt odio necessitatibus aliquam doloremque incidunt, quo id alias optio laborum? ",
-        name: "ipsum dolor",
-        email: "xyz@gmail.com"
+        img_src: "../Assets/chloe.jpg",
+        text: "The team behind it truly understands the needs of developers and consistently delivers high-quality content.",
+        name: "Max Muller",
+        email: "maxmuller@gmail.com"
     },
     {
-        img_src: "../Assets/himesh.jpg",
-        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti sunt odio necessitatibus aliquam doloremque incidunt, quo id alias optio laborum? ",
-        name: "ipsum dolor",
-        email: "xyz@gmail.com"
+        img_src: "../Assets/chloe.jpg",
+        text: "This Newsletter has expanded my technical knowledge but also inspired me to explore new areas and experiment with different technologies.",
+        name: "Johannasen",
+        email: "johannasen@gmail.com"
     },
 
 ]
 
 export default testimonialList;
+

406f7eb54190b1a7fd94de9d0d8d53de27e274fd
Bikram <bikrambehera14440@gmail.com>
2023-06-03 23:22:04 +0530

    Improve Faq Section

--- server/index.js
+++ server/index.js
@@ -1,29 +1,27 @@
 const express = require('express');
-const cors = require('cors')
-const db =require('./db');
-const PORT = 5000;
-let Validator = require('validatorjs');
+const cors = require('cors');
+const db = require('./db');
+const Validator = require('validatorjs');
+
 const app = express();
-const route = require('./routes/routes');
-const router = express.Router();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
 app.use(express.json());
 app.use(cors());
 
+// Routes
+const routes = require('./routes/routes');
+app.use('/api', routes);
 
-app.use("/api",route);
-
-app.get("/",(req,res) => {
-    res.json({
-        message:"welcome to this server"
-    });
+// Default route
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to this server',
+  });
 });
 
-
-
-
-
-
-
+// Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on 5000`)
-  });
+  console.log(`Server is running on port ${PORT}`);
+});
